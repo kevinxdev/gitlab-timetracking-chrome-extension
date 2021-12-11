3f072b7a29564458cb5231339150d94ce4e70608
Kevin <kevin@gigaclub.net>
2021-12-11 17:34:32 +0100

    added options, layout and basic functionality

---
+++ src/js/options.js
@@ -0,0 +1,58 @@
+let gitlabUrlInput = document.getElementById('gitlab-url');
+let gitlabPATInput = document.getElementById('gitlab-pat');
+
+function constructSettings() {
+    chrome.storage.sync.get(['gitlabUrl', 'gitlabPAT'], function(data) {
+        if (data.gitlabUrl) {
+            gitlabUrlInput.value = data.gitlabUrl;
+        }
+        if (data.gitlabPAT) {
+            gitlabPATInput.value = data.gitlabPAT;
+        }
+        gitlabUrlInput.addEventListener('change', function() {
+            chrome.storage.sync.set({'gitlabUrl': this.value});
+            let request = new Request(`${this.value}api/v4/projects`, {});
+            fetch(request)
+                .then(response => {
+                    if (gitlabUrlInput.classList.contains('is-invalid')) {
+                        gitlabUrlInput.classList.remove('is-invalid');
+                        document.getElementById("validationServer03Feedback").classList.add("invisible");
+                    }
+                })
+                .catch(error => {
+                    gitlabUrlInput.classList.add("is-invalid");
+                    document.getElementById("validationServer03Feedback").classList.remove("invisible");
+                })
+        });
+        gitlabPATInput.addEventListener('change', function() {
+            chrome.storage.sync.set({'gitlabPAT': this.value});
+            chrome.storage.sync.get('gitlabUrl', ({gitlabUrl}) => {
+                let request = new Request(`${gitlabUrl}api/v4/user`, {
+                    headers: {
+                        'PRIVATE-TOKEN': this.value
+                    }
+                });
+                fetch(request)
+                    .then(response => {
+                        if (gitlabPATInput.classList.contains('is-invalid')) {
+                            gitlabPATInput.classList.remove('is-invalid');
+                            document.getElementById("validationServer04Feedback").classList.add("invisible");
+                        }
+                        response.json().then(data => {
+                            if (data.message == "401 Unauthorized") {
+                                gitlabPATInput.classList.add("is-invalid");
+                                document.getElementById("validationServer04Feedback").classList.remove("invisible");
+                            } else {
+                                chrome.storage.sync.set({'gitlabUserID': data.id});
+                            }
+                        })
+                    })
+                    .catch(error => {
+                        gitlabPATInput.classList.add("is-invalid");
+                        document.getElementById("validationServer04Feedback").classList.remove("invisible");
+                    })
+            });
+        });
+    });
+}
+constructSettings();
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,49 @@
 let dashboardButton = document.getElementById('dashboard-button');
 
 dashboardButton.addEventListener('click', function () {
-     chrome.tabs.create({url: chrome.runtime.getURL("src/html/index.html")});
+     chrome.tabs.create({url: chrome.runtime.getURL("src/html/dashboard.html")});
 });
+
+function loadIssues() {
+    chrome.storage.sync.get(['gitlabUrl', 'gitlabPAT', 'gitlabUserID'], function(data) {
+        if (data.gitlabUrl && data.gitlabPAT && data.gitlabUserID) {
+            let request = new Request(`${data.gitlabUrl}api/v4/issues?assignee_id=${data.gitlabUserID}`, {
+                headers: {
+                    'PRIVATE-TOKEN': data.gitlabPAT
+                }
+            });
+            fetch(request)
+                .then(response => response.json())
+                .then(data => {
+                    let issues = data.filter(issue => issue.state === 'opened');
+                    console.log(issues);
+                    let issueTable = document.getElementById('issue-table');
+                    issues.forEach(issue => {
+                        let issueRow = document.createElement('tr');
+                        let issueReference = document.createElement('td');
+                        issueReference.innerText = issue.references.short;
+                        issueRow.appendChild(issueReference);
+                        let issueTitle = document.createElement('td');
+                        let issueLink = document.createElement('a');
+                        issueLink.href = issue.web_url;
+                        issueLink.target = '_blank';
+                        issueLink.innerText = issue.title;
+                        issueTitle.appendChild(issueLink);
+                        issueRow.appendChild(issueTitle);
+                        let issueState = document.createElement('td');
+                        issueState.innerText = issue.state;
+                        issueRow.appendChild(issueState);
+                        let issueTimeSpent = document.createElement('td');
+                        issueTimeSpent.innerText = issue.time_stats.total_time_spent;
+                        issueRow.appendChild(issueTimeSpent);
+                        let issueSelected = document.createElement('td');
+                        issueSelected.innerText = "SELECTED";
+                        issueRow.appendChild(issueSelected);
+                        issueTable.appendChild(issueRow);
+                    });
+                });
+        }
+    });
+}
+
+loadIssues();
